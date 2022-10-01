91a1f3ad942d797e76de26f07c78433019aadf6f
MohammedAlThobaiti <63994695+MohammedAlThobaiti@users.noreply.github.com>
2022-10-01 14:26:56 +0300

    Delete FG14-Hopin-CPIT499-main/FG14-Hopin-CPIT499-main directory

--- FG14-Hopin-CPIT499-main/FG14-Hopin-CPIT499-main/services/database.dart
+++
@@ -1,8 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class Database {
-  //collection ref
-
-  final  CollectionReference communityList = FirebaseFirestore.instance.collection('CommunityList');
-
-}
